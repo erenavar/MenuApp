5914acce873ba1e074ed1937bf59462deb3c7996
Eren AVAR <eren.avar@hotmail.com>
2024-01-23 11:34:10 +0300

    Created Category Component
    Fetched categories from dummy data to CategoryGrid

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,17 @@
 import { FlatList, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { CATEGORIES } from "../data/dummy-data";
+import CategoryGrid from "../components/CategoryGrid";
 
 export default function CategoriesScreen() {
-  function renderCategoryItem(item) {
-    console.log(item);
+  function renderCategoryItem(itemData) {
+    return (
+      <CategoryGrid title={itemData.item.title} color={itemData.item.color} />
+    );
   }
 
   return (
     <View>
-
       <FlatList
         data={CATEGORIES}
         keyExtractor={(item) => item.id}
