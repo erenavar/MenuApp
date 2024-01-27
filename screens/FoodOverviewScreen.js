9d58e3cd3e6a7680da68d9099e748de5a6f103ab
Eren AVAR <eren.avar@hotmail.com>
2024-01-27 20:28:06 +0300

    Sent Category Id to Related Page

--- screens/FoodOverviewScreen.js
+++ screens/FoodOverviewScreen.js
@@ -1,10 +1,11 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-export default function FoodOverviewScreen() {
+export default function FoodOverviewScreen({ route }) {
+  const categoryId = route.params.categoryId;
   return (
     <View>
-      <Text>FoodOverviewScreen</Text>
+      <Text>FoodOverviewScreen - {categoryId}</Text>
     </View>
   );
 }
