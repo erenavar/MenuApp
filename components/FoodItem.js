8beee7b003afefb634707082332defb9a1353ee5
Eren AVAR <eren.avar@hotmail.com>
2024-01-29 20:45:07 +0300

    Styled FoodItem Component

--- components/FoodItem.js
+++ components/FoodItem.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
 export default function FoodItem({
@@ -9,10 +9,57 @@ export default function FoodItem({
   affordability,
 }) {
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.foodItem}>
+      <Pressable
+        style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+      >
+        <View>
+          <View>
+            <Image style={styles.image} source={{ uri: imageUrl }} />
+            <Text style={styles.title}>{title} </Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{complexity}</Text>
+            <Text style={styles.detailItem}>{affordability}</Text>
+          </View>
+        </View>
+      </Pressable>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  foodItem: {
+    margin: 15,
+    backgroundColor: "white",
+    elevation: 4,
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    borderRadius: 10,
+  },
+  image: {
+    height: 200,
+    width: "100%",
+  },
+  title: {
+    fontSize: 18,
+    textAlign: "center",
+    fontWeight: "bold",
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 5,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+});
