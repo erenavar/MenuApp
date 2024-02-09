9311d7dd8f9cda3378834237b90b79e53926e408
Eren AVAR <eren.avar@hotmail.com>
2024-02-09 13:09:25 +0300

    Added Icon to Drawer Items

--- App.js
+++ App.js
@@ -5,8 +5,38 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import FoodOverviewScreen from "./screens/FoodOverviewScreen";
 import FoodDetailScreen from "./screens/FoodDetailScreen";
+import FavoritesScreen from "./screens/FavoritesScreen";
+import { Entypo } from "@expo/vector-icons";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function Root() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: "rebeccapurple" },
+        headerTintColor: "white",
+      }}
+    >
+      <Drawer.Screen
+        name="Categories"
+        component={CategoriesScreen}
+        options={{
+          title: "All Categories",
+          drawerIcon: () => <Entypo name="list" size={20} color="black" />,
+        }}
+      />
+      <Drawer.Screen
+        name="Favorites"
+        component={FavoritesScreen}
+        options={{
+          drawerIcon: () => <Entypo name="star" size={20} color="black" />,
+        }}
+      />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -18,13 +48,21 @@ export default function App() {
           contentStyle: { backgroundColor: "lightblue" },
         }}
       >
-        <Stack.Screen
+        {/* <Stack.Screen
           name="Categories"
           component={CategoriesScreen}
           options={{
             title: "All Categories",
           }}
+        /> */}
+        <Stack.Screen
+          name="Deneme"
+          component={Root}
+          options={{
+            headerShown: false,
+          }}
         />
+
         <Stack.Screen name="FoodOverview" component={FoodOverviewScreen} />
         <Stack.Screen
           name="FoodDetail"
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function FavoritesScreen() {
+  return (
+    <View>
+      <Text>Favorites Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
